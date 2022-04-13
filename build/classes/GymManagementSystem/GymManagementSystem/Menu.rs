905e4677cbb99ede71611a2731edc929bafc0e8a
David John Bas <95739092+djhbas@users.noreply.github.com>
2022-04-13 17:53:26 +0800

    Delete build/classes/GymManagementSystem directory

--- build/classes/GymManagementSystem/GymManagementSystem/Menu.rs
+++
@@ -1,7 +0,0 @@
-javaproject.Menu
-javaproject.Menu$6
-javaproject.Menu$3
-javaproject.Menu$2
-javaproject.Menu$5
-javaproject.Menu$4
-javaproject.Menu$1
--- build/classes/GymManagementSystem/GymManagementSystem/addMembers.rs
+++
@@ -1,8 +0,0 @@
-javaproject.addMembers
-javaproject.addMembers$7
-javaproject.addMembers$6
-javaproject.addMembers$5
-javaproject.addMembers$4
-javaproject.addMembers$3
-javaproject.addMembers$2
-javaproject.addMembers$1
